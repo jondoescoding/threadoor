e40cad0b7f7bcd1277b1cc2696fe90ea934eaa93
jondidathing <jondidathing.jm@gmail.com>
2023-05-18 09:45:46 -0500

    updates to main

--- src/threadoor/main.py
+++ src/threadoor/main.py
@@ -1,7 +1,6 @@
 # Python 
 import os
 # Langchain
-#from langchain.llms import OpenAI
 from langchain.llms import *
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
@@ -11,18 +10,21 @@ import helper as hp
 
 # ENVIRONMENT VARIABLES
 OPENAI_TOKEN = os.environ.get('openAi')
-
+HUGGINGFACE_TOKEN = os.environ.get('huggingFace')
 
 # Setting up LLMS
 
 # OpenAi
-openAiLLM = OpenAI(openai_api_key=OPENAI_TOKEN, temperature=0.7, max_tokens=500)
+#llm = OpenAI(openai_api_key=OPENAI_TOKEN, temperature=0.7, max_tokens=500)
+
+# Testing HuggingFaceHub
+llm = HuggingFaceHub(repo_id=)
 
 # ROLES BEGIN HERE -> Writer, Editor, SEO, Writer, Prompt Engineer (Optional) 
 
 # Writer -> 1st
 writer = hp.Role(
-    llm=openAiLLM,
+    llm=llm,
     template="""
     Role: You are a professional freelance copy writer. You have the ability to explain difficult topics down to their fundamentals using first principle thinking applying the Feynman Technique. Your tone is similar to the author Mark Manson.
     Objective: Given an topic for a blog post you are to write a first draft of the target topic.
@@ -34,7 +36,7 @@ writer = hp.Role(
 
 # Editor -> 2nd
 editor = hp.Role(
-    llm=openAiLLM,
+    llm=llm,
     template="""
     Role: You are a professional Editor who was worked in  the online written industry for over 10 years. 
     Objective: Given a draft for a blog post you are to provide a bulleted list of feedback and advice based on the area of expertise
@@ -45,7 +47,7 @@ editor = hp.Role(
 
 # SEO -> 3rd
 seo = hp.Role(
-    llm=openAiLLM,
+    llm=llm,
     template="""
     Role: You are a professional SEO marketer who has been working for over 10 years.
     Objective: Given a draft for a blog post, you are to provide a bulleted list feedback and advice on how to improve the SEO
@@ -55,7 +57,7 @@ seo = hp.Role(
 )
 
 production = hp.Role(
-    llm=openAiLLM,
+    llm=llm,
     template="""
     Role: You are a professional copywriter.
     Objective: Given a blog draft you are exptected to use the feedback from an editor and changes to seo keywords to construct a blog post.
@@ -69,7 +71,7 @@ production = hp.Role(
 # CHAINS BEGIN HERE
 
 writerChain = writer.createChain(
-    llm=openAiLLM,
+    llm=llm,
     promptTemplate=writer.setPromptTemplate(
         ["topic", "expertise"],
         template=writer.template
@@ -78,7 +80,7 @@ writerChain = writer.createChain(
 )
 
 editorChain = editor.createChain(
-    llm=openAiLLM,
+    llm=llm,
     promptTemplate=editor.setPromptTemplate(
         ["draft"],
         template=editor.template
@@ -87,7 +89,7 @@ editorChain = editor.createChain(
 )
 
 seoChain = seo.createChain(
-    llm=openAiLLM,
+    llm=llm,
     promptTemplate=seo.setPromptTemplate(
         ["draft"],
         template=seo.template
@@ -96,7 +98,7 @@ seoChain = seo.createChain(
 )
 
 productionChain = production.createChain(
-    llm=openAiLLM,
+    llm=llm,
     promptTemplate=production.setPromptTemplate(
         ["draft", "edit", "seo"],
         template=production.template
